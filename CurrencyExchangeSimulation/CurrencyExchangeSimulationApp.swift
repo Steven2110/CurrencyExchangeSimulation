8af846d5ca51d1a53f8252f5937862375132a226
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-02-23 22:23:35 +0700

    Initial Commit

---
+++ CurrencyExchangeSimulation/CurrencyExchangeSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeSimulationApp.swift
+//  CurrencyExchangeSimulation
+//
+//  Created by Steven Wijaya on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
